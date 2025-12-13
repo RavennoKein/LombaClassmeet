cb9659be142067e42a8fbbb17a5760549800925d
ArkanaRaven <ramandaivan12@gmail.com>
2025-12-13 20:13:28 +0700

    ubah keywoard di navbar

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@ const SUPABASE_BUCKET = "website-sekolah";
 
 const DAGANGAN_BUCKET = "dagangan";
 
-const DIFY_CHATBOT_URL = "https://udify.app/chatbot/vZOqc4H987xMYq3o";
+const DIFY_CHATBOT_URL = "https://udify.app/chat/fFlGt5DDrsZOgCKs";
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
